0dec92ab486c5c8c208434f04e4bbc168c6669b0
AlexanderUp <alexander.uperenk0@yandex.ru>
2022-12-03 02:03:49 +0300

    Models added

--- bonus/cards/admin.py
+++ bonus/cards/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Card, CardSeries, Transaction
+
+
+class CardSeriesAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "description",
+    )
+    list_display_links = (
+        "description",
+    )
+    search_fields = (
+        "description",
+    )
+    empty_value_display = "--empty--"
+
+
+class CardAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "series",
+        "number",
+        "issue_date",
+        "card_duration_type",
+        "last_used_date",
+        "balance",
+        "status",
+        "valid_until",
+    )
+    list_select_related = (
+        "series",
+    )
+    search_fields = (
+        "series__description",
+    )
+    list_filter = (
+        "issue_date",
+        "status",
+    )
+    empty_value_display = "--empty--"
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "card",
+        "amount",
+        "description",
+    )
+
+
+admin.site.register(CardSeries, CardSeriesAdmin)
+admin.site.register(Card, CardAdmin)
+admin.site.register(Transaction, TransactionAdmin)
--- bonus/cards/models.py
+++ bonus/cards/models.py
@@ -1,3 +1,146 @@
-from django.db import models
+from datetime import timedelta
+from decimal import Decimal
 
-# Create your models here.
+from django.core.validators import MinValueValidator
+from django.db import models
+from django.utils import timezone
+
+
+class CardSeries(models.Model):
+    description = models.CharField(
+        max_length=100,
+        verbose_name="description",
+        help_text="Card series description",
+    )
+
+    class Meta:
+        verbose_name = "card_series"
+        verbose_name_plural = "card_serieses"
+        ordering = ("-id",)
+
+    def __str__(self):
+        return f"CardSeries({self.pk:0>5})"
+
+
+class Card(models.Model):
+    ONE_MONTH = timedelta(days=30)
+    SIX_MONTH = timedelta(days=183)
+    ONE_YEAR = timedelta(days=365)
+
+    NOT_ACTIVATED = 0
+    ACTIVATED = 1
+    OUTDATED = 2
+
+    CARD_DURATION_CHOICES = [
+        (ONE_MONTH, 'One month'),
+        (SIX_MONTH, 'Six month'),
+        (ONE_YEAR, 'One year'),
+    ]
+
+    CARD_STATUS_CHOICES = [
+        (NOT_ACTIVATED, "Not activated"),
+        (ACTIVATED, "Active"),
+        (OUTDATED, "Outdated"),
+    ]
+
+    series = models.ForeignKey(
+        CardSeries,
+        related_name="cards",
+        on_delete=models.CASCADE,
+        verbose_name="series",
+        help_text="ID of card series",
+    )
+    number = models.PositiveIntegerField(
+        verbose_name="number",
+        help_text="card number",
+        validators=(MinValueValidator(1, "Card numbers start from 1."),),
+    )
+    issue_date = models.DateTimeField(
+        default=timezone.now,
+        verbose_name="issue_date",
+        help_text="Card issue date",
+    )
+    card_duration_type = models.DurationField(
+        verbose_name="card_duration_type",
+        help_text="Card duration type",
+        choices=CARD_DURATION_CHOICES,
+        default=ONE_MONTH,
+    )
+    last_used_date = models.DateTimeField(
+        verbose_name="last_used_date",
+        help_text="Date of last card usage",
+        null=True,
+        blank=True,
+    )
+    balance = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="balance",
+        help_text="Card balance",
+        validators=[MinValueValidator(0, "Balance can not be negative"),],
+        default=Decimal(0.0),
+    )
+    status = models.PositiveSmallIntegerField(
+        verbose_name="status",
+        help_text="Card status",
+        choices=CARD_STATUS_CHOICES,
+        default=NOT_ACTIVATED,
+    )
+
+    class Meta:
+        verbose_name = "card"
+        verbose_name_plural = "cards"
+        ordering = ("-id",)
+        constraints = (
+            models.CheckConstraint(
+                check=models.Q(balance__gte=Decimal(0.0)),
+                name="balance_is_not_negative"
+            ),
+            models.UniqueConstraint(
+                fields=("series", "number"),
+                name="series_number_unique_constraint"
+            ),
+        )
+
+    def __str__(self):
+        return f"Card({self.series.pk:0>5}-{self.number:0>7})"
+
+    @property
+    def valid_until(self):
+        return (self.issue_date + self.card_duration_type)
+
+
+class Transaction(models.Model):
+    card = models.ForeignKey(
+        Card,
+        related_name="transactions",
+        on_delete=models.CASCADE,
+        verbose_name="card",
+        help_text="card",
+    )
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="amount",
+        help_text="Transaction amount",
+        default=Decimal(0.0),
+    )
+    description = models.CharField(
+        max_length=100,
+        verbose_name="description",
+        help_text="Transaction description",
+    )
+
+    class Meta:
+        verbose_name = "transaction"
+        verbose_name_plural = "transactions"
+        ordering = ("-id",)
+        constraints = (
+            models.CheckConstraint(
+                check=~models.Q(amount=Decimal(0.0)),
+                name="zero_amount_transaction"
+            ),
+        )
+
+    def __str__(self):
+        return f"Transaction({self.amount})"
